76e2e4390eea2bd9ea6b2b9df655e7bce55d080e
Marco Caponera <marco.caponera4@gmail.com>
2025-10-22 14:20:55 +0200

    Explosive barrel, Enemy
    - Explosive barrel works with new damage system
    - More enemy bug fixing

--- Source/BO/Public/DamageSystem/DamageClass.h
+++ Source/BO/Public/DamageSystem/DamageClass.h
@@ -10,5 +10,6 @@ enum class EDamageClass : uint8
 	None = 0 UMETA(DisplayName = "None"),
 	Melee = 1 UMETA(DisplayName = "Melee"),
 	Projectile = 2 UMETA(DisplayName = "Projectile"),
-	Hazard = 3 UMETA(DisplayName = "Hazard")
+	Hazard = 3 UMETA(DisplayName = "Hazard"),
+	Explosion = 4 UMETA(DisplayName = "Explosion")
 };
